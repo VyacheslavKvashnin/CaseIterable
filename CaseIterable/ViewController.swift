7ce9bc1dd62419d7e721ac0f9671cfbd644e8ae1
komplektan <komplektan@mail.ru>
2022-08-16 19:25:55 +0500

    s and f

--- CaseIterable/ViewController.swift
+++ CaseIterable/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Menu: String, CaseIterable {
+        case one = "One"
+        case two = "Two"
+        case three = "Three"
+    }
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.frame = view.bounds
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Menu.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = Menu.allCases[indexPath.row].rawValue
+        return cell
+    }
+}
